3ab60c89ce411579aa6c64d316c205f21ab9d567
espnumrodez <157631492+espnumrodez@users.noreply.github.com>
2024-01-25 11:44:50 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-local-de-rption",
+      "name": "Local de répétion",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 0.9730155701218575,
+        "pitch": 0.16366406361065167,
+        "fov": 1.3337680586201845
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "local-repetition",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
